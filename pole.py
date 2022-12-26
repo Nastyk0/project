627d74820cc5eb17000bd0d595d8ec312bcb09d4
sammira2 <sammira.isangulova@yandex.ru>
2022-12-26 16:06:31 +0300

    клетчатое поле

---
+++ pole.py
@@ -0,0 +1,36 @@
+import pygame
+
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        self.left = 0
+        self.top = 0
+        self.cell_size = 30
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                pygame.draw.rect(screen, (255, 255, 255),
+                                 (x * self.cell_size + self.left, y * self.cell_size + self.left,
+                                  self.cell_size, self.cell_size), 1)
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+
+size = 390, 390
+screen = pygame.display.set_mode(size)
+board = Board(15, 15)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    board.render(screen)
+    pygame.display.flip()
